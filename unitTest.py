4259b053a0f095616c6a113e3fc17c7673731dfe
MasonPles <masonples@gmail.com>
2021-03-22 19:52:44 -0600

    New unit test (NotEquals POS)

--- unitTest.py
+++ unitTest.py
@@ -4,12 +4,15 @@ from Interface import reponse as res
 
 
 class TestSum(unittest.TestCase):
-    
+
     def testStem(self):
         self.assertEqual(pi.psStem("Marcus decided he wasn\'t going to come"), "marcu decid he wasn't go to come " , "Expected: marcu decid he wasn\'t go to come")
 
     def testPOS(self):
         self.assertEqual(pi.getPOS("Marcus"), 'NN', "Expected: NN")
+
+    def testPOS2(self):
+        self.assertNotEqual(pi.getPOS("Walking"), 'NN', "Expected: Not Equal")
 
     def testName(self):
         self.assertEqual(pi.interpolate(2, "Marcus"), "1Robert: Marcus? That's a good name!\n" , "Expected: 1Robert: Marcus? That's a good name!\n")
